7ebda2ff9dae2b9f2c04e0cee5323c66a83ad6f2
Abbas Parpia <aparpia03@gmail.com>
2020-02-17 18:23:18 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -14,8 +14,10 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
+       
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+    programmer = programmer_hash[:dennis_ritchie][:languages]
+    programmer.join("")
 end
 
 def adding_matz
@@ -63,6 +70,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+
 
 	programmer_hash = 
  		{
@@ -80,6 +88,8 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = {:known_for =>"Ruby", :Languages =>["LISP", "C"]}
+programmer_hash
     
 end
 
@@ -101,7 +111,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -121,5 +132,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] =["C", "Assembly"]
+programmer_hash
 end
